de37f7677b055cf644893f08b4cdb69ac0a5d1c0
Carlos Saraiva <carlos.saraiva@msn.com>
2017-07-28 22:40:42 -0300

    configuring webpack development server

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,22 @@ var webpack = require('webpack');
 var path = require('path')
 
 var config = {
-  context: path.resolve(__dirname, 'src'),
+  context: path.join(__dirname, 'src'),
   entry: {
     app: './app.js',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'), 
-    publicPath: "/assets/",
+    path: path.join(__dirname, 'dist'), 
+    publicPath: '/assets',
     filename: '[name].bundle.js',
   },
+  devServer: {
+    open: true,
+    compress: true,
+    hot: false,    
+    contentBase: path.join(__dirname, 'src'),
+    openPage: ''
+  }
 };
 
 module.exports = config;
